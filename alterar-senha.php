e1309da26790dfd73b71d2b62ba4c3a483c353d1
Vitor Fernando da Silva <vitorfernando.silva@campus.labs>
2022-10-10 20:18:21 -0900

    add header in separate file

--- alterar-senha.php
+++ alterar-senha.php
@@ -21,31 +21,7 @@
     </style>
   </head>
 <body class="text-center bg-light bodycss">  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
-      
-    <div class="container-fluid">
-      <a class="navbar-brand" href="home.html">
-        <img class="mb-4" src="assets/ifrs.png" alt="" width="45" height="50">ADS Carteiras
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="d-flex">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item dropdown btn btn-md btn-success">
-              <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Perfil
-              </a>
-              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="user-edit.html">Alterar perfil</a></li>
-                <li><a class="dropdown-item" href="alterar-senha.html">Alterar senha</a></li>
-              </ul>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>   
+  <?php include_once('header.php'); ?>  
   <main class="form-signin w-100 m-auto">
     <form>
       <h1 class="h3 mt-4 mb-3 fw-normal">Alterar senha</h1>
