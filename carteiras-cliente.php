f77f9967e4d3bc077e80dbcdf73e006f11e8833a
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2022-10-26 21:48:31 -0300

    info cliente carteiras

--- carteiras-cliente.php
+++ carteiras-cliente.php
@@ -1,3 +1,41 @@
+<?php
+
+require_once('session-analista.php');
+require_once('banco.php');
+
+// Verifica se o parametro esta no get
+if (!array_key_exists("id", $_GET)) {
+  header('location: list-clientes.php');
+}
+
+$id_cliente = $_GET['id'];
+
+// Pega o token do cliente
+$queryToken = mysqli_query($mysqli,
+    "SELECT id_token FROM usuario WHERE id='$id_cliente'"
+  );
+
+  if ($queryToken && ($result = mysqli_fetch_assoc($queryToken)) && (mysqli_num_rows($queryToken) > 0)) {
+    $token = $result['id_token'];
+    
+    // Verifica se o token e do analista
+    $queryAnalista = mysqli_query($mysqli,
+      "SELECT id_analista FROM token WHERE id='$token'"
+    );
+
+    if ($queryAnalista && ($result = mysqli_fetch_assoc($queryAnalista))) {
+      $analista = $result['id_analista'];
+      
+      if ($analista <> $_SESSION['id']) {
+        header('location: list-clientes.php');
+      }
+    }
+  } else {
+    header('location: list-clientes.php');
+  }
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -19,12 +57,21 @@
         <div class="row mb-3">
           <div class="col-md-12">
             <div class="h-100 p-5 text-white bg-secondary bg-gradient rounded-3">
-              <h2>Nome e sobrenome</h2>
-              <br>
-              <p>Endereço: Endereço</p>
-              <p>Email: Email</p>
-              <p>CPF: RG</p>
-              <p>RG: RG</p>
+              <?php 
+                $queryCliente = mysqli_query($mysqli,
+                  "SELECT * FROM usuario WHERE id='$id_cliente'"
+                );
+            
+                if ($queryCliente && ($result = mysqli_fetch_assoc($queryCliente))) {
+              ?>
+                <h2><?=$result['nome'] . ' ' . $result['sobrenome'];?></h2>
+                <br>
+                <p>Endereço: <?=$result['endereco'];?></p>
+                <p>Email: <?=$result['email'];?></p>
+                <p>RG: <?=$result['rg'];?></p>
+              <?php } else {
+                header('location: list-clientes.php');
+              } ?>
             </div>
         </div>
     </div>
@@ -33,7 +80,6 @@
           <a href="carteira.php">
             <div class="h-100 p-5 text-white bg-success bg-gradient rounded-3">
               <h5>Carteira A</h5>
-              <p>Valor Total: R$ 1234.56</p>
             </div>
           </a>
         </div>
